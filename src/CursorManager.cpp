2107e0d255f0be4b68fb61ab63014ef149a7bda0
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2026-02-17 13:52:36 -0800

    Delete ShowHideRealCursor.cpp

--- src/CursorManager.cpp
+++ src/CursorManager.cpp
@@ -1,7 +1,9 @@
 #include "CursorManager.hpp"
+#include "Geode/cocos/platform/win32/CCEGLView.h"
 #include "Geode/ui/OverlayManager.hpp"
 #include "Cursor.hpp"
 #include <Geode/binding/GameManager.hpp>
+#include <Geode/binding/PlatformToolbox.hpp>
 
 void CursorManager::updateOrInit() {
     auto gameManager = GameManager::get();
@@ -31,13 +33,27 @@ void CursorManager::updateOrInit() {
     this->m_cursor->setAnchorPoint(ccp(1.0f, 0.5f));
 
     this->m_cursor->setRotation(240);
-    this->m_cursor->setScale(0.5);
+    this->m_cursor->setScale(0.55);
 }
 
 void CursorManager::updateToPosition() {
+    if (!CCEGLView::get()->getShouldHideCursor()) {
+        PlatformToolbox::hideCursor();
+    }
     this->m_cursor->setPosition(getMousePos());
+
+    // eclipse moment
+    bool canShowInLevel = true;
+    if (auto* playLayer = PlayLayer::get()) {
+        canShowInLevel = playLayer->m_hasCompletedLevel ||
+                          playLayer->m_isPaused ||
+                          GameManager::get()->getGameVariable("0024");
+    }
+
+    this->visible(canShowInLevel);
 }
 
 void CursorManager::visible(bool state) {
     this->m_cursor->setVisible(state);
 }
+

b73d4bf8c243da1c9cb2fae36cd3a0182bff6532
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2026-02-25 20:59:41 -0800

    "s" :fire:

--- src/platform/PlatformShared.cpp
+++ src/platform/PlatformShared.cpp
@@ -6,3 +6,8 @@ using namespace geode::prelude;
 bool PlatformManager::getIsHidden() {
     return this->m_hidden;
 }
+
+void PlatformManager::setCursorVisibility(bool visible) {
+    this->m_hidden = visible;
+    this->resetCursor();
+}

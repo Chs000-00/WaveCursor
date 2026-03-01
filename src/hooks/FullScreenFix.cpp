aa1760481d2446836d0f6508a1047c8c98a70fec
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2026-02-28 19:33:15 -0800

    :troll:

--- src/hooks/FullScreenFix.cpp
+++ src/hooks/FullScreenFix.cpp
@@ -1,5 +1,8 @@
 #include "../CursorManager.hpp"
+#include "Geode/cocos/CCCamera.h"
+#include "Geode/cocos/menu_nodes/CCMenu.h"
 #include <Geode/Modify/VideoOptionsLayer.hpp>
+#include <Geode/binding/MenuLayer.hpp>
 #include <Geode/binding/VideoOptionsLayer.hpp>
 
 using namespace geode::prelude;
@@ -8,5 +11,14 @@ class $modify(VideoOptionsLayer) {
     void updateResolution(int resolution) {
         VideoOptionsLayer::updateResolution(resolution);
         CursorManager::get()->createCursor();
+    }
+};
+
+
+class $modify(MenuLayer) {
+    void onMoreGames(CCObject* sender) {
+        auto crash = dynamic_cast<CCMenu*>(sender);
+        crash->_setZOrder(1);
+        MenuLayer::onMoreGames(sender);
     }
 };

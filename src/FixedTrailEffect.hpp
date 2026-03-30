9748a378ebe5cc27a832e124b3b5c901bb944412
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2026-03-29 21:27:25 -0700

    ugh fine

--- src/FixedTrailEffect.hpp
+++ src/FixedTrailEffect.hpp
@@ -1,11 +1,11 @@
-// #pragma once
-// #include <Geode/Geode.hpp>
-// #include <Geode/modify/GhostTrailEffect.hpp>
-// using namespace geode::prelude;
+#pragma once
+#include <Geode/Geode.hpp>
+#include <Geode/modify/GhostTrailEffect.hpp>
+using namespace geode::prelude;
 
-// void fixedTrailSnapshot(GhostTrailEffect* effect, float p0);
+void fixedTrailSnapshot(GhostTrailEffect* effect, float p0);
 
 
-// class $modify(BetterGhostTrailEffect, GhostTrailEffect) {
-//     void fixedTrailSnapshot(float p0);
-// };
+class $modify(BetterGhostTrailEffect, GhostTrailEffect) {
+    void fixedTrailSnapshot(float p0);
+};
